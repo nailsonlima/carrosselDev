da095a1c5e622020f7036345e88b49977bb48dc3
Nailson <nailsonlima131@gmail.com>
2022-10-24 20:06:42 -0300

    carrossel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react'
 import './App.css';
+import { motion } from 'framer-motion'
+import { CgArrowsExchangeAlt } from 'react-icons/cg'
+
+import image1 from './images/1.jpg'
+import image2 from './images/2.jpg'
+import image3 from './images/3.jpg'
+import image4 from './images/4.jpg'
+
+const images = [image1, image2, image3, image4]
 
 function App() {
+  const carousel = useRef();
+
+  const [width, setWidth] = useState(0);
+
+  useEffect(()=>{
+    console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='header'>
+      <p>CARROSSEL</p><CgArrowsExchangeAlt className='icon-seta'/>
     </div>
+    <div className='App'>
+      <motion.div ref={carousel} className='carrossel' whileTap={{cursor: 'grabbing'}}>
+        <motion.div 
+        className='inner'
+        drag="x"
+        dragConstraints={{right: 0, left: -width}}
+        initial={{x:150}}
+        animate={{x:0}}
+        transition={{duration: 0.8}}
+        >
+          
+          {images.map(image => (
+            <motion.div className='item' key={image}>
+              <img width="200" height="260" src={image}/>
+            </motion.div>
+          ))}
+
+        </motion.div>
+      </motion.div>
+    </div>
+    </>
   );
 }
 
